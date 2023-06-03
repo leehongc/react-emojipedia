dde0e6752b79f4b2be38021f150a140766e62fb0
Hong C <36454250+leehongc@users.noreply.github.com>
2023-06-02 23:01:41 -0700

    console log short version of each meaning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import emojipedia from "./emojipedia";
+
+const shortMeaning = emojipedia.map(function(entry) {
+    return entry.meaning.substring(0,100)
+});
+
+console.log(shortMeaning);
 
 ReactDOM.render(<App />, document.getElementById("root"));
