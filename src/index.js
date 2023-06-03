bb0fee3ce683a76bc4ec0a8d10b076ed1ced09f3
Hong C <36454250+leehongc@users.noreply.github.com>
2023-06-02 23:27:11 -0700

    simply map function with arrow functions

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import emojipedia from "./emojipedia";
 
-const shortMeaning = emojipedia.map(function(entry) {
-    return entry.meaning.substring(0,100)
-});
+const shortMeaning = emojipedia.map(entry => entry.meaning.substring(0,100));
 
 console.log(shortMeaning);
 
