2a2fb0d4e02b94d317bea3f766b56a03bc1096e8
Hong C <36454250+leehongc@users.noreply.github.com>
2023-12-28 17:53:23 -0800

    starting this lesson over

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Entry from "./Entry";
-import emojipedia from "../emojipedia";
-
 
 function App() {
   return (
@@ -11,14 +8,44 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-        {emojipedia.map(emojiTerm =>
-          (<Entry 
-            key = {emojiTerm.id}
-            emoji = {emojiTerm.emoji}
-            name = {emojiTerm.name}
-            meaning ={emojiTerm.meaning}
-            />)
-          )}
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              💪
+            </span>
+            <span>Tense Biceps</span>
+          </dt>
+          <dd>
+            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
+            used in connection with doing sports, e.g. at the gym.
+          </dd>
+        </div>
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🙏
+            </span>
+            <span>Person With Folded Hands</span>
+          </dt>
+          <dd>
+            Two hands pressed together. Is currently very introverted, saying a
+            prayer, or hoping for enlightenment. Is also used as a “high five”
+            or to say thank you.
+          </dd>
+        </div>
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🤣
+            </span>
+            <span>Rolling On The Floor, Laughing</span>
+          </dt>
+          <dd>
+            This is funny! A smiley face, rolling on the floor, laughing. The
+            face is laughing boundlessly. The emoji version of “rofl“. Stands
+            for „rolling on the floor, laughing“.
+          </dd>
+        </div>
       </dl>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import emojipedia from "./emojipedia";
-
-const shortMeaning = emojipedia.map(entry => entry.meaning.substring(0,100));
-
-console.log(shortMeaning);
 
 ReactDOM.render(<App />, document.getElementById("root"));
